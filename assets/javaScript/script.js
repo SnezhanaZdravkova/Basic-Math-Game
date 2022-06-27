43841e40d41692875284559599ce9e53a2eac1b8
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-06-27 10:12:45 +0000

    Add semicolon to line 29 and 32

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -19,14 +19,14 @@ checkBtn.addEventListener("click", function(){
     let userInput = parseInt(inputEl.value) // number
 
     if(userInput === 2){
-        modal.style.backgrownd = "rgb(46, 204, 113)";
+        modal.style.background = "rgb(46,204, 113)";
        theAnswer.textContent = answerOptions[0];
        }else{
         modal.style.background = "#e74c3c"
        theAnswer.textContent = answerOptions[1];
     }
     
-})
+});
 closeBtn.addEventListener("click", function(){
     modal.style.display = "none";
-})
+});
